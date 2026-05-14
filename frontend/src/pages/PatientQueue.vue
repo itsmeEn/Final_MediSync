85564c31be6dac3c3517b1e02b6b693d6b337be1
EucliffeStrauss <angelorodel13@gmail.com>
2026-05-15 03:31:20 +0800

    Revert "Fix PatientQueue pre-intake button click handler type"
    This reverts commit 559431fa9c42c79497c0e9c78d16844a3e9be96c.

--- frontend/src/pages/PatientQueue.vue
+++ frontend/src/pages/PatientQueue.vue
@@ -312,7 +312,7 @@
                   unelevated
                   :loading="preIntakeSubmitting"
                   :disable="!canSubmitPreIntake || preIntakeSubmitting"
-                  @click="() => void submitPreIntake()"
+                  @click="submitPreIntake"
                 />
               </div>
             </q-card-section>
